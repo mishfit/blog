b698cb2250c3affabb90d8cd6a99b9a695362752
mishfit <mishochu@gmail.com>
2018-08-10 15:17:10 -0600

    add deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 /* globals process */
-const less            = require('gulp-less'),
+const { decrypt }     = require('exec-pgp'),
+      gulp            = require('gulp'),
+      less            = require('gulp-less'),
       plumber         = require('gulp-plumber'),
+      rename          = require('gulp-rename'),
       uglifycss       = require('gulp-uglifycss')
 
 gulp.task('less', function () {
@@ -9,10 +12,24 @@ gulp.task('less', function () {
 
   return gulp.src(lessFiles)
     .pipe(plumber())
-    .pipe(less)
-    .pipe(uglifycss)
+    .pipe(less())
+    .pipe(uglifycss())
+    .pipe(rename({ suffix: '.min' }))
     .pipe(plumber.stop())
     .pipe(gulp.dest(cssDirectory))
+})
+
+gulp.task('deploy', function () {
+  decrypt('./.deploy.json.asc')
+    .then(result => {
+      const { output } = result,
+            config = JSON.parse(output),
+            sourceDir = config.source + '/*`
+      
+      return gulp.src(sourceDir)
+        .pipe(plumber())
+        .pipe(gulp.dest(config.dest))
+    })
 })
 
 gulp.task('build', ['less'])
