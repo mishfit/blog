9d84440102699e6b3a456389ad74d52768519b2d
mishfit <mishochu@gmail.com>
2018-08-15 05:49:53 -0600

    add build step

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('deploy', function () {
     .then(result => {
       const { output } = result,
             config = JSON.parse(output),
-            sourceDir = config.source + '/*`
+            sourceDir = config.source + '/*'
       
       return gulp.src(sourceDir)
         .pipe(plumber())
